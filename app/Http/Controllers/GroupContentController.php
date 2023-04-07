c5b9598bffea88f7a0cd1010ff5ebe660d69a320
guolam <guolam123@gmail.com>
2023-04-07 10:54:16 +0000

    showdetail debug

--- app/Http/Controllers/GroupContentController.php
+++ app/Http/Controllers/GroupContentController.php
@@ -135,7 +135,7 @@ public function showdetail($id)
         }
         } else {
         // 存在しないIDの場合のリダイレクト先
-        return redirect()->route('dashboard');
+        return redirect()->route('group.index');
         
         }
     }
